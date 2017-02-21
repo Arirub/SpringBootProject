37a27514aeed8b9d7e3ac4a24817e77a2cefb33d
ariane <ariane@arianes-mbp>
2017-02-21 20:01:49 +0100

    Add Implementation

--- backendninja/src/main/java/com/udemy/controller/LoginController.java
+++ backendninja/src/main/java/com/udemy/controller/LoginController.java
@@ -1,38 +1,34 @@
 package com.udemy.controller;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.udemy.model.UserCredential;
+import com.udemy.constant.ViewConstant;
 
 @Controller
 public class LoginController {
 
-	@GetMapping("/")
-	public String redirectToLogin(){
-		return "redirect:/login";
-	}
-	
+	private static final Log LOG = LogFactory.getLog(LoginController.class);
+		
 	@GetMapping("/login")
 	public String showLoginForm(Model model,
 			@RequestParam(name="error", required=false) String error, 
 			@RequestParam(name="logout", required=false) String logout){
+		LOG.info("METHOD: showLoginForm() -- PARAMS:  error="+ error + ", logout:"+ logout);
 		model.addAttribute("error", error);
 		model.addAttribute("logout",logout);
-		model.addAttribute("userCredentials", new UserCredential());
-		return "login";
+		LOG.info("Returning to login view");
+		return ViewConstant.LOGIN;
 	}
 
-	@PostMapping("/logincheck")
-	public String loginCheck(@ModelAttribute(name="userCredentials") UserCredential userCredential){
-		if(userCredential.getUsername().equals("user") && userCredential.getPassword().equals("user")){
-			return "contacts";
-		}
-		return "redirect:/login?error";
-		
+	@GetMapping({"/loginsuccess","/"})
+	public String loginCheck(){
+		LOG.info("METHOD: loginCheck()");
+		LOG.info("Returning to contacts view");
+		return "redirect:/contacts/showcontacts";
 	}
 }
